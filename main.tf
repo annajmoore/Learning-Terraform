7b312ecc1a0cefc306a28f1c00040a1fc8e787cc
annajmoore <anna.j.moore@outlook.com>
2026-05-28 00:31:06 -0400

    Added a tag to Azure resource

--- main.tf
+++ main.tf
@@ -31,6 +31,11 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Enviroment = "Terraform Getting Started w/ Azure"
+    Team       = "DevOps"
+  }
 }
 
 
